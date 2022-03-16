5ba80c0a3e621066a8744981264ae5e5da688bde
AtenrevCode <atenrev@gmail.com>
2022-03-16 20:35:39 +0100

    I hate brs

--- src/pages/index.js
+++ src/pages/index.js
@@ -123,7 +123,7 @@ const IndexPage = () => {
               │  ├─ <a href="https://play.google.com/store/apps/details?id=com.HUMANINPUTS.FINGERIT">fingerit.apk</a><br />
               │  ├─ <a href="https://humaninputs.itch.io/hackterm">hackterm.exe</a><br />
               ├─ <a href="https://github.com/Atenrev/Deep-Image-Steganography-Reimplementation">Deep-Image-Steganography-Reimplementation.py</a><br />
-              ├─ <a href="https://github.com/Atenrev/forocoches-language-generation">forocoches-language-generation.py</a>
+              ├─ <a href="https://github.com/Atenrev/forocoches-language-generation">forocoches-language-generation.py</a><br />
               ├─ <a href="https://github.com/Atenrev/chessformers">chessformers.py</a>
             </p>
           </div>
